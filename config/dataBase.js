d74f17bc571a8cd4e6667219c49b54e81ac1759a
vudu79 <vudu1979@gmail.com>
2020-03-25 00:58:45 +1200

    изменил db

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://andrey:SpkSpkSpk1979!!!@cluster0-br2kb.mongodb.net/test?retryWrites=true&w=majority',
+    db: 'mongodb+srv://andrey:12345@cluster0-z2ijq.mongodb.net/test?retryWrites=true&w=majority',
     secret: 'eny_secret_key'
 }
